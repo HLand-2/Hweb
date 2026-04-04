644bdb34e6ed49d5f9a64273b3089463f2d6396b
Turbo-Code1000 <henry.t.wilcock@gmail.com>
2026-04-04 18:11:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const topics = [
+    "",
+    "",
+    "",
+    ""
+]
+const data = [
+    "",
+    "",
+    "",
+    ""
+]
 
+document.getElementById("t1").innerText = topics[0];
+document.getElementById("t11").innerText = data[0]
+
+document.getElementById("t2").innerText = topics[1];
+document.getElementById("t22").innerText = data[1]
+
+document.getElementById("t3").innerText = topics[2];
+document.getElementById("t33").innerText = data[2]
+
+document.getElementById("t4").innerText = topics[3];
+document.getElementById("t44").innerText = data[3]
