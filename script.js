b2444992d3b700956fc7b0f5f56fb7004b069e5c
Turbo-Code1000 <henry.t.wilcock@gmail.com>
2026-04-04 19:28:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,29 @@ const data = [
     "",
     ""
 ]
+const style = {
+    mdbc: "Main background colour",
+    mdtc: "Main text colour",
+    mtbc: "Title background colour",
+    mttc: "Title text colour",
+    stbc: "Subtitle background colour",
+    sttc: "Subtitle text colour"
+}
+const styles = `
+body {
+    background-color: ${style[mdbc]};
+    color: ${style[mdtc]};
+}
 
+h1 {
+    background-color: ${style[mtbc]};
+    color: ${style[mttc]};
+}
+h2 {
+    background-color: ${style[stbc]};
+    color: ${style[sttc]};
+}
+`
 document.getElementById("t1").innerText = topics[0];
 document.getElementById("t11").innerText = data[0]
 
@@ -22,3 +44,5 @@ document.getElementById("t33").innerText = data[2]
 
 document.getElementById("t4").innerText = topics[3];
 document.getElementById("t44").innerText = data[3]
+
+document.getElementById("sl").innerText = styles
