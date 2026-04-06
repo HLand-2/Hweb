3f30d6f486ab062e15b04efd40cf60c1fe6e6833
Turbo-Code1000 <henry.t.wilcock@gmail.com>
2026-04-06 08:59:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,16 +35,16 @@ h2 {
 }
 `
 document.getElementById("t1").innerText = topics[0];
-document.getElementById("t11").innerText = data[0]
+document.getElementById("t11").innerText = data[0];
 
 document.getElementById("t2").innerText = topics[1];
-document.getElementById("t22").innerText = data[1]
+document.getElementById("t22").innerText = data[1];
 
 document.getElementById("t3").innerText = topics[2];
-document.getElementById("t33").innerText = data[2]
+document.getElementById("t33").innerText = data[2];
 
 document.getElementById("t4").innerText = topics[3];
-document.getElementById("t44").innerText = data[3]
+document.getElementById("t44").innerText = data[3];
 
 document.getElementById("sl").innerText = styles;
 document.getElementsByClassName("t").innerText = `All about ${name}`;
